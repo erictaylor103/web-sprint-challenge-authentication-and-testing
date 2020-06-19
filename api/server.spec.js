573bea47c29e43cbcebcb7d89632496ee93f6651
Eric Taylor <erictaylor@Erics-MBP.attlocal.net>
2020-06-19 19:20:19 -0400

    added 1 test for register and one more test for login

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,6 +3,17 @@ const supertest = require("supertest");
 const server = require("./server.js");
 const db = require("../database/dbConfig.js");
 
+const userDetails = {
+    username: "eric",
+    password: "123",
+  };
+
+  
+const createUser = async (userDetails) => {
+    return await supertest(server).post("/api/auth/register").send(userDetails);
+  }; 
+  
+
 it("should user the testing environment", () => {
     expect(process.env.DB_ENV).toBe("testing");
 });
@@ -66,28 +77,38 @@ describe("server.js", () => {
     })
 
 
+//check for status 201 when a user is registered
+it("It should return status code of 201", () => {
+    return supertest(server)
+      .post("/api/auth/register")
+      .send(userDetails)
+      .then((res) => {
+        expect(res.status).toBe(201);
+      });
+  });
 
-
-
-
-    //test that the user login is not undefined
-
-    //it('POST /api/auth/login', () => {
-    //    return supertest(server)
-    //        .post('/api/auth/login')
-    //        .send({ username: 'john', password: '123' })
-    //        .then(res => {
-    //            expect(res.body.token).not.toBeUndefined()
-    //        })
-    //})
+//check if a token is returned on login
+    it("Should return a token", async () => {
+        await createUser(userDetails);
     
+        const res = await supertest(server)
+          .post("/api/auth/login")
+          .send(userDetails);
+    
+        return expect(res.body.token).toBeTruthy();
 
+        
 
+      });
 
-
-
-
+      //check for status 201 when registering a user
+      
+      
+      
 
 
 
 });
+
+
+
