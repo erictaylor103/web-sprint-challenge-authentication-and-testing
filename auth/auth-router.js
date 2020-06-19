3f14d0d49bd3bf3129964ea06ec72cffae7b7d40
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-06-19 13:56:25 -0400

    user register, login, authentication middleware working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,90 @@
+const bcryptjs = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
+//router.post('/register', (req, res) => {
   // implement registration
+//});
+
+//router.post('/login', (req, res) => {
+  // implement login
+//});
+
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
+const constants = require("../config/constants.js");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        console.log("user:", user);
+
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = createToken(user);
+
+          res.status(200).json({ token, message: "Welcome to our API" }); //send the token and the message
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
+  }
 });
+
+//create a token function
+
+function createToken(user){
+  const payload = { //payload is the data we want to store inside the token
+    subject: user.id,
+    username: user.username,
+    
+  };
+
+  //the secret is being imported from ../config/constants
+  const secret = constants.jwtSecret; //the secret is to sign the token to make sure we can verify the token when it cmes back
+
+  const options = {
+    expiresIn: '1d', //this means 1 day // this is the expiration timeframe for the token
+  };
+
+  return jwt.sign(payload, secret, options); //the sign method will return a new token that we will have access to
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,38 @@
   before granting access to the next middleware/route handler
 */
 
+//module.exports = (req, res, next) => {
+//  res.status(401).json({ you: 'shall not pass!' });
+//};
+
+const jwt = require('jsonwebtoken'); //needs the library to access the "verify" method
+
+const constants = require("../config/constants.js");
+
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  // add code here to verify users are logged in
+
+  const token = req.headers.authorization; //read the authorization header
+  const secret = constants.jwtSecret; //the secret is imported from ../config/constants.js
+
+    if(token){
+      jwt.verify(token, secret, (error, decodedToken) =>{ //callback function must be an arrow function
+        if(error){
+          //the token is invalid
+          res.status(401).json({message: "you cannot pass"});
+        }else{
+          req.decodedToken = decodedToken;
+
+          next();
+        }
+      });
+      
+    }else{
+      res.status(401).json({message: "please provide credentials"})
+    }
+
+
+  
 };
+
